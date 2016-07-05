e98b9ad081523c97f5c9b0470e000d8c14a6cffa
vishnukey <vishnukey@gmail.com>
2016-07-04 19:38:23 -0600

    a better UI!

--- hay.c
+++ hay.c
@@ -3,21 +3,21 @@
 
 int main (void)
 {
-	printf("How big is this thing?\n");
+	printf("How big is this thing? ");
 	char* b =malloc(3*sizeof(char));
 	fgets(b, 3, stdin);
 	int length = atoi(b);
 	int nums [length]; 
 	for(int i =0; i < length; i++)
 	{
-		printf("Give me a number\n");
+		printf("Give me a number ");
 		char* t = malloc(3*sizeof(char));
 		fgets(t, 3, stdin);
 		int temp = atoi(t);
 		free(t);
 		nums[i] = temp;
 	}
-	printf("Now, what number are you looking for?\n");
+	printf("Now, what number are you looking for? ");
 	char* g = malloc(3*sizeof(char));
 	fgets(g, 2, stdin);
 	int goal = atoi(g);
